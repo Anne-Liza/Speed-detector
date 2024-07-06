cf6cd1e25a31d14954fe59b0d33f888681059c9b
Anne-Liza <annewangui210@gmail.com>
2024-07-06 16:42:53 +0300

    Updates index.js file
    Updates file correcting syntax errors,

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-//calculate demerit points
-function calculatedemeritpoints(speed){
-    
-    const demeritPointsPerRange = 1;
-    const speedLimit = 70;
-
-    let excessSpeed = (speed - speedLimit);
-    if (excessSpeed <=0){
-        return "Your speed is Ok.";
-    }
-
-    let demeritPoints = Math.floor(excessSpeed / 5);
-    if (demeritPoints <= 12) {
-        return `Your speed is ${speed}. You have received ${demeritPoints} demerits.`;
-    }
-
-    else {
-        return `Your speed is ${speed}.Your license has been suspended.`;
-    }
-}
-    console.log (calculatedemeritpoints(80));
-    console.log (calculatedemeritpoints(104));
-    console.log (calculatedemeritpoints(40));
-    console.log (calculatedemeritpoints(64));
-    console.log (calculatedemeritpoints(98));
-    console.log (calculatedemeritpoints(298));
-
 document.getElementById("speedForm").addEventListener("submit",function(e){
     e.preventDefault();
+    
+    //speed from form
+    const speed = +document.getElementById("speed").value;
 
-    let speed = document.getElementById("speed").value;
-    let notification = calculateDemeritPoints(speed);
-this.document.getElementById("result").innerText
- }
-)
+    //calculate demerit points
+    function calculateDemeritPoints(speed){
+    
+        const demeritPointsPerRange = 1;
+        const speedLimit = 70;
+
+        let excessSpeed = speed - speedLimit;
+        if (excessSpeed <=0){
+            return "Your speed is Ok.";
+        }
+
+        let demeritPoints = Math.floor(excessSpeed / 5);
+        if (demeritPoints <= 12) {
+            return `Your speed is ${speed} km/hr. You have received ${demeritPoints} demerits.`;
+        }
+
+        else {
+            return `Your speed is ${speed} km/hr. Your license has been suspended.`;
+        }
+
+    }
+    //display results
+    const resultMessage = calculateDemeritPoints(speed);
+    document.getElementById("carspeed").innerText =`${speed} km/hr`;
+    document.getElementById("result").innerText = resultMessage;
+
+});
 
 
 
